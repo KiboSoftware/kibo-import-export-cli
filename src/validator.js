5ba136d4e279090defa786f322c9d3ff4f50f962
thomas.phipps <thomas.phipps@kibocommerce.com>
2023-10-18 08:24:33 -0500

    wip

--- src/validator.js
+++ src/validator.js
@@ -6,7 +6,7 @@ import path from 'path';
 import { parse } from 'csv-parse';
 
 import CatalogFetcher from './catalog-fetcher.js';
-import Joi from 'joi';
+import Joi  from 'joi';
 
 class Validator {
   constructor(catalogFetch, directory) {
@@ -20,7 +20,9 @@ class Validator {
     
     this.directory = path.resolve(directory || '.');
     this.fileMap = {
+      'attributes.csv': this.validateAttributes,
       'producttypeattributes.csv': this.validateproductTypeAttributes,
+      'producttypeattributevalues.csv': this.validateProductTypeAttributeValues,
     };
   }
   loadState(){
@@ -205,14 +207,171 @@ class Validator {
     }
     return joyKey;
   }
+  booleanValidator(required = true) {
+    let val =  Joi.string().lowercase().valid('true', 'false','yes','no');
+    if ( required ) {
+      return val.required();
+    }
+    return val.allow('');
+  }
 
+  async validateAttributes(){
+    //MasterCatalogName,AttributeCode,AttributeAdminName,AttributeName,Description,DataType,InputType,IsExtra,IsOption,IsProperty,Namespace,SearchableInStorefront,SearchableInAdmin,SearchDisplayValue,AvailableForOrderRouting
+    const { parser, file } = this.getParser('attributes.csv');
+    if (!parser) {
+      return false;
+    }
+    const tenant = await this.catalogFetch.getTenant();
+    const attributes = await this.getAttributeMap();
+    const attributeCodeSchema = this.toDependandStringSetValidator(attributes);
+    const attributeAdminNameSchema = Joi.string().required();
+    const attributeNameSchema = Joi.string().required();
+    const descriptionSchema = Joi.string().allow('');
+    const dataTypeSchema = Joi.string()
+      .required()
+      .lowercase()
+      .valid('string', 'number', 'bool', 'datetime', 'productcode');
+    const inputTypeSchema = Joi.string()
+      .required()
+      .lowercase()
+      .valid(
+        'list',
+        'textbox',
+        'yesno',
+        'textarea',
+        'datetime',
+        'date',
+      );
+    const isExtraSchema = this.booleanValidator();
+    const isOptionSchema = this.booleanValidator();
+    const isPropertySchema = this.booleanValidator();
+    const namespaceSchema = Joi.string().required();
+    const searchableInStorefrontSchema = this.booleanValidator();
+    const searchableInAdminSchema = this.booleanValidator();
+    const searchDisplayValueSchema = Joi.string().required();
+    const availableForOrderRoutingSchema = this.booleanValidator();
+    const schema = Joi.object({
+      mastercatalog: Joi.number().integer().required(),
+      attributecode: attributeCodeSchema,
+      attributeadminname: attributeAdminNameSchema,
+      attributename: attributeNameSchema,
+      description: descriptionSchema,
+      datatype: dataTypeSchema,
+      inputtype: inputTypeSchema,
+      isextra: isExtraSchema,
+      isoption: isOptionSchema,
+      isproperty: isPropertySchema,
+      namespace: namespaceSchema,
+      searchableinstorefront: searchableInStorefrontSchema,
+      searchableinadmin: searchableInAdminSchema,
+      searchdisplayvalue: searchDisplayValueSchema,
+      availablefororderrouting: availableForOrderRoutingSchema,
+    }).unknown(true);
+
+    for await (const entry of parser) {
+      entry.record.mastercatalog =
+        entry.mastercatalog ??
+        tenant.maps.masterCatalgByName[
+          entry.record.mastercatalogname?.toLowerCase() || ''
+        ]?.id;
+      let value = schema.validate(entry.record, { debug: false });
+      if (value.error) {
+        value.error.details.forEach((err) => {
+          this.logError({ file, line: entry.info.lines, message: err.message });
+        });
+        return false;
+      }
+    }
+
+    this.logInfo({ file, line: -1, message: `valid` });
+
+  }
   async validateProductTypeAttributeValues() {
     const { parser, file } = this.getParser('productTypeAttributeValues.csv');
     if (!parser) {
       return false;
     }
 
+    //MasterCatalogName,ProductType,AttributeCode,Type,VocabularyValue
+
+    
+    const attributes = await this.getAttributeMap();
+    const productTypes = await this.getProducTypeMap();
+    const attributeCodeSchema = this.toDependandStringSetValidator(attributes);
+    const productTypeSchema = this.toDependandStringSetValidator(productTypes);
+    const propAttributes = await this.getAttributes({
+      responseFields:
+        'items(attributeCode,attributeFQN,isOption,isExtra,isProperty)',
+    });
+    const typeSchema = Joi.string()
+      .required()
+      .lowercase()
+      .custom((value, helpers) => {
+        let record = helpers.state.ancestors;
+        let mc = record[0].mastercatalog;
+        let att = propAttributes[mc].find(
+          (att) =>
+            att.attributeCode.toLowerCase() ===
+            record[0].attributecode.toLowerCase(),
+        );
+        if (att != null) {
+          switch (value) {
+            case 'property':
+              if (!att.isProperty) {
+                return helpers.message('attribute doesnt support isProperty');
+              }
+              break;
+            case 'option':
+              if (!att.isOption) {
+                return helpers.message('attribute doesnt support isOption');
+              }
+              break;
+            case 'extra':
+              if (!att.isExtra) {
+                return helpers.message('attribute doesnt support isExtra');
+              }
+              break;
+            case 'variantproperty':
+              if (!att.isProperty) {
+                return helpers.message('attribute doesnt support isProperty');
+              }
+              break;
+            default:
+              return helpers.message('invalid type');
+          }
+        }
+        return value;
+      });
+
+    const vocabValueSchema = Joi.string()
+      .required();
+
+    const schema = Joi.object({
+      mastercatalog: Joi.number().integer().required(),
+      producttype: productTypeSchema,
+      attributecode: attributeCodeSchema,
+      type: typeSchema,
+      vocabularyvalue:vocabValueSchema
+    }).unknown(true);
+
     const tenant = await this.catalogFetch.getTenant();
+
+    for await (const entry of parser) {
+      entry.record.mastercatalog =
+        entry.mastercatalog ??
+        tenant.maps.masterCatalgByName[
+          entry.record.mastercatalogname?.toLowerCase() || ''
+        ]?.id;
+      let value = schema.validate(entry.record, { debug: false });
+      if (value.error) {
+        value.error.details.forEach((err) => {
+          this.logError({ file, line: entry.info.lines, message: err.message });
+        });
+        return false;
+      }
+    }
+
+    this.logInfo({ file, line: -1, message: `valid` });
   }
 
   async validateproductTypeAttributes() {
